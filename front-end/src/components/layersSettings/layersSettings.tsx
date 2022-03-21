e523a4169d2d1630f3a020ebaa6ba8523869e4da
Vitaly <msk.vitaly@gmail.com>
2022-03-21 18:36:25 +0300

    [MVP1] react-beautiful-dnd

--- front-end/src/components/layersSettings/layersSettings.tsx
+++ front-end/src/components/layersSettings/layersSettings.tsx
@@ -1,5 +1,6 @@
 import React, { Dispatch, SetStateAction } from 'React'
 import IContentPage from '../../interfaces/IContentPage'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import styles from './LayersSettings.module.scss'
 
 type Props = {
@@ -12,14 +13,30 @@ const LayersSettings = ({ contentPage, currentLayer, setCurrentLayer }: Props) =
 
   return (<aside className={styles.layersSettingsContainer}>
     <h1>Настройки слоёв</h1>
-    <div>
-      {contentPage.layers.map((x, i) =>
-        <div key={i}
-          onClick={() => setCurrentLayer(i)}
-          className={`${styles.layerCard} ${i === currentLayer ? styles.layerActive : ""}`}>
-          {x.content[0].url}
-        </div>)}
-    </div>
+    {/* Колонки */}
+    <DragDropContext onDragEnd={console.log}>
+      {/* 1 колонка */}
+      <Droppable droppableId='layers'>
+        {({ droppableProps, innerRef }) => <div ref={innerRef} {...droppableProps}>
+          {/* Карточки */}
+          {contentPage.layers.map((x, i) =>
+            <Draggable key={i}
+              index={i}
+              draggableId={`${i}`}>
+              {/* 1 карточка */}
+              {({ draggableProps, innerRef, dragHandleProps }) =>
+                <div
+                  // onClick={() => setCurrentLayer(i)}
+                  className={`${styles.layerCard} ${i === currentLayer ? styles.layerActive : ""}`}
+                  ref={innerRef}
+                  {...draggableProps}
+                  {...dragHandleProps}>
+                  <p>{x.content[0].url}</p>
+                </div>}
+            </Draggable>)}
+        </div>}
+      </Droppable>
+    </DragDropContext>
     <button className={styles.addLayerBtn}>Добавить слой</button>
   </aside>)
 }
