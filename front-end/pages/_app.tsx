ff574d964c2e307b3840b17a687c17323ac25c28
Vitaly <msk.vitaly@gmail.com>
2022-04-01 04:43:32 +0300

    [MVP1] debug yandex-metrics

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,18 +1,21 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { YMInitializer } from 'react-yandex-metrika';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-    {/* <!-- Yandex.Metrika counter -->*/}
-    <YMInitializer version="2" accounts={[88113924]} options={{
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-      webvisor: true,
-      trackHash: true
-    }} />
-    {/* <!-- /Yandex.Metrika counter --> */}
+    <Head>
+      {/* Yandex.Metrika counter */}
+      <YMInitializer version="2" accounts={[88113924]} options={{
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+        trackHash: true
+      }} />
+      {/* /Yandex.Metrika counter */}
+    </Head>
     <Component {...pageProps} />
   </>
 }
