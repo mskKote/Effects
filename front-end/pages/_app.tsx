5eeb693a508f492139ef65b8c0770361cbb17d67
mskKote <msk.vitaly@gmail.com>
2024-03-28 04:17:52 +0100

    infra: test deployment

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Router from "next/router";
-import Loader from "../src/components/loader/Loader";
+// import Loader from "../src/components/loader/Loader";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = React.useState(false);
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     };
   }, []);
 
-  if (loading) return <Loader />;
+  if (loading) return <>LOADING...</>;
 
   return <Component {...pageProps} />;
 }
